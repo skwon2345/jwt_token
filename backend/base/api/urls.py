4278b6ae54464c016f9c763638268e4b9d4aab32
sukkwono <sukkwono@sfu.ca>
2023-02-11 20:54:24 -0800

    edit urls.py to use jwt

--- backend/base/api/urls.py
+++ backend/base/api/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from . import views
 
-urlpatterns = [path("", views.getRoutes)]
+urlpatterns = [
+    path("", views.getRoutes),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
