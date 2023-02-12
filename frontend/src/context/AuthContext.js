98e6523b13b4cfd3ecaa7847e013b3014d99a724
sukkwono <sukkwono@sfu.ca>
2023-02-12 12:24:48 -0800

    update access token every 4 minutes.

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -11,6 +11,7 @@ export const AuthProvider = ({children}) => {
     // get authTokens from localStorage first to check if it is there. else just null.
     let [authTokens, setAuthTokens] = useState(() => localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null);
     let [user, setUser] = useState(() => localStorage.getItem('authTokens') ? jwt_decode(localStorage.getItem('authTokens')) : null);
+    let [loading, setLoading] = useState(true)
 
     const navigate = useNavigate();
 
@@ -51,11 +52,46 @@ export const AuthProvider = ({children}) => {
         navigate('/login')
     }
 
+    let updateToken = async () => {
+        console.log('Update Token log')
+        let response = await fetch("http://localhost:8000/api/token/refresh/", {
+            method:'POST',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({'refresh':authTokens.refresh})
+        })
+
+        let data = await response.json()
+        
+        if (response.status === 200) {
+            setAuthTokens(data);
+            setUser(jwt_decode(data.access));
+            localStorage.setItem('authTokens', JSON.stringify(data));
+        } else {
+            logoutUser();
+        }
+    }
+
     let contextData = {
         user:user,
         loginUser: loginUser,
         logoutUser: logoutUser
     };
+
+    useEffect(()=> {
+
+        let fourMinutes = 1000 * 600 * 4
+
+        let interval = setInterval(()=>{
+            if (authTokens) {
+                updateToken()
+            }
+        }, fourMinutes)
+
+        return () => clearInterval(interval)
+
+    }, [authTokens, loading])
 
     return(
         <AuthContext.Provider value={contextData}>
