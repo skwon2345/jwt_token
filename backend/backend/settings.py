496e67e9f76551350bcd5498096767a483c7e4af
sukkwono <sukkwono@sfu.ca>
2023-02-11 20:49:37 -0800

    jwt settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -46,6 +46,13 @@ INSTALLED_APPS = [
     "rest_framework",
 ]
 
+# Use JWT as Authentication
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
