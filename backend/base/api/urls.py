ce66035c402eca38f9e499f8a23b8565ab15d64f
sukkwono <sukkwono@sfu.ca>
2023-02-11 21:35:43 -0800

    use MyTokenObtainPairView to apply the customized token claims

--- backend/base/api/urls.py
+++ backend/base/api/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from . import views
 
 urlpatterns = [
     path("", views.getRoutes),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path(
+        "token/",
+        views.MyTokenObtainPairView.as_view(),
+        name="token_obtain_pair",
+    ),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
