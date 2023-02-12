c733627d1043bfc0bcbd1934a2f34b600cf5ce52
sukkwono <sukkwono@sfu.ca>
2023-02-11 23:58:52 -0800

    display login page depends on user infromation from AuthContext

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,15 +3,19 @@ import { Link } from 'react-router-dom'
 import AuthContext from '../context/AuthContext'
 
 const Header = () => {
-  let {name} = useContext(AuthContext)
-  
+  let {user} = useContext(AuthContext)
+
   return (
     <div>
         <Link to="/">Home</Link>
         <span> | </span>
-        <Link to="/login">Login</Link>
-
-        <p>Hello {name}</p>
+        {user ? (
+          <p>Logout</p>
+        ) : (
+          <Link to="/login">Login</Link>
+        )}
+        
+        {user && <p>Hello {user.username}</p>}
     </div>
   )
 }
