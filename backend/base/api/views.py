83561be4835e9685ef85cfe8072d2c7043607209
sukkwono <sukkwono@sfu.ca>
2023-02-11 20:45:09 -0800

    init

---
+++ backend/base/api/views.py
@@ -0,0 +1,13 @@
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(["GET"])
+def getRoutes(request):
+    routes = [
+        "/api/token",
+        "/api/token/refresh",
+    ]
+
+    return Response(routes)
