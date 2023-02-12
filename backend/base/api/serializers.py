567e3769ed070316a7be0f9b2fb433abbbe5a3ba
sukkwono <sukkwono@sfu.ca>
2023-02-12 12:36:10 -0800

    add Note model to backend

---
+++ backend/base/api/serializers.py
@@ -0,0 +1,8 @@
+from base.models import Note
+from rest_framework.serializers import ModelSerializer
+
+
+class NoteSerializer(ModelSerializer):
+    class Meta:
+        model = Note
+        fields = "__all__"
